62d925d3f74543e90341d5db0560f64e60042215
Sena Çelik <sena.celik@miateknoloji.com>
2023-03-27 20:17:32 +0300

    color changed

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color.fromARGB(255, 152, 236, 121);
-const kPrimaryLightColor = Color.fromARGB(255, 119, 255, 128);
+const kPrimaryColor = Color.fromRGBO(2, 89, 64, 1);
+const kPrimaryLightColor = Color.fromRGBO(2, 89, 64, 1);
